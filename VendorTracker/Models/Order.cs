47d25e239846a302c0b4934709079e1ca8e97af1
Josh Tillinghast <jwtill@icloud.com>
2022-03-04 10:59:36 -0800

    setDescription pass test can set

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,7 +5,7 @@ namespace VendorTracker.Models
   public class Order
   {
     public string Title { get; set; }
-    public string Description { get;  }
+    public string Description { get; set; }
     public double Price { get;  }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
