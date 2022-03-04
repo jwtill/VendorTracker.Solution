931bd1aee51c3858271fcb4d008c8a03283dc643
Josh Tillinghast <jwtill@icloud.com>
2022-03-04 15:42:35 -0800

    Good fail AddOrder associates order with vendor. No AddOrder yet

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -47,5 +47,20 @@ namespace VendorTracker.Tests
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
+    [TestMethod]
+  public void AddOrder_AssociatesOrderWithVendor_OrderList()
+  {
+    string title = "Blue Monday Order"; 
+    string description = "Some Things";
+    double price = 3.99;
+    Order newOrder = new Order(title, description, price);
+    List<Order> newList = new List<Order> { newOrder };
+    string name2 = "Special Agent Cooper";
+    string description2 = "RR Diner";
+    Vendor newVendor = new Vendor(name2, description2);
+    newVendor.AddOrder(newOrder);
+    List<Order> result = newVendor.Orders;
+    CollectionAssert.AreEqual(newList, result);
+  }
   }
 }
