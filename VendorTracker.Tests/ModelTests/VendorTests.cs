621dcf8621a541f39fe45056776097a740124e3a
Josh Tillinghast <jwtill@icloud.com>
2022-03-04 15:23:55 -0800

    Good fail getAll returns all vendor objects

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -3,14 +3,14 @@ using VendorTracker.Models;
 using System.Collections.Generic;
 using System;
 
-namespace VendorTracker.Tests 
+namespace VendorTracker.Tests
 {
   [TestClass]
   public class VendorTests : IDisposable
   {
     public void Dispose()
     {
-      TestCategoryAttribute.ClearAll();
+      Vendor.ClearAll();
     }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
@@ -26,13 +26,26 @@ namespace VendorTracker.Tests
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
-     [TestMethod]
+    [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       string description = "A smelly place";
       Vendor newVendor = new Vendor("Amazing Larry", description);
       string result = newVendor.Description;
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name1 = "Amazing Larry";
+      string name2 = "Jason Robards";
+      string description1 = "Herman Foods";
+      string description2 = "That Restaurant";
+      Vendor newVendor1 = new Vendor(name1, description1);
+      Vendor newVendor2 = new Vendor(name2, description2);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
