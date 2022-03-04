63708dc8ed26d95d431a17e45906f55722410990
Josh Tillinghast <jwtill@icloud.com>
2022-03-04 11:05:40 -0800

    Good fail setPrice cannot set price because it is read only

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -71,5 +71,17 @@ namespace VendorTracker.Tests
       string result = newOrder.Description;
       Assert.AreEqual(updatedDescription, result);
     }
+    [TestMethod]
+    public void SetPrice_SetPrice_Double()
+    {
+      string title = "Blue Monday Order"; 
+      string description = "Some Things";
+      double price = 3.99;
+      Order newOrder = new Order(title, description, price);
+      double updatedPrice = 7.99;
+      newOrder.Price = updatedPrice;
+      double result = newOrder.Price;
+      Assert.AreEqual(updatedPrice, result);
+    }
   }
 }
